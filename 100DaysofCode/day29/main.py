4818278e86168b0d40b62024b1dc1a9be4f85666
Juaaanits <juanitoramos113@gmail.com>
2025-05-27 08:31:30 +0800

    feat: added gui

--- 100DaysofCode/day29/main.py
+++ 100DaysofCode/day29/main.py
@@ -191,39 +191,12 @@ from PIL import Image, ImageTk
 '''
 
 from tkinter import *
-import tkinter as tk
-from PIL import Image, ImageTk
+from tkinter import messagebox
+from random import choice, randint, shuffle
+#import pyperclip
 
 
-window = tk.Tk()
-canvas = tk.Canvas(window, width=200, height=200)
-canvas.pack()
-
-image = Image.open("logo.png")
-photo = ImageTk.PhotoImage(image)
-
-canvas.create_image(100, 100,image=photo)
-
-window.title("Password Manager")
-
-w = Label(window, text="Password Manager")
-w.pack(padx=100, pady=25)
-
-window.mainloop()
-
-'''
-window.pack(padx=25, pady=25)
-
-c = Canvas(window, 200, 200)
-
-image = Image.open('logo.png')
-
-photo = ImageTk.PhotoImage(image)
-
-label = tk.label(window, image=photo)
-''''''
-
-
+#Part 2:Implementing Password Functionality
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                'u', 'v',
@@ -233,6 +206,53 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_list = [choice(letters) for _ in range(randint)]
-    pass
-    '''
+    password_list = [choice(letters) for _ in range(randint(8,10))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    shuffle(password_list)
+    password = ''.join(password_list)
+
+def save():
+    website = website.entry.get()
+    email_username = email_username_label.get()
+    password = password_entry.get()
+
+    if len(password) == 0 or len(email_username) == 0 or len(website) == 0:
+        messagebox.showwarning(title="Empty fields", message="Please don't leave any empty fields")
+        return
+    
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=25, pady=25)
+
+canvas = Canvas(window, width=200, height=200)
+photo = PhotoImage(file='logo.png')  # Ensure you have a logo.png file in the same directory
+canvas.create_image(100, 100,image=photo)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+email_username_label = Label(text="Email/Username:")
+email_username_label.grid(column=0, row=2)
+password_label = Label(text="Password:", width=21)
+password_label.grid(column=0, row=3)
+
+generate_button = Button(text="Generate Password", width=21, command=generate_password)
+generate_button.grid(column=2, row=3, columnspan=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+email_username_label = Entry(width=35)
+email_username_label.grid(column=1, row=2, columnspan=2)    
+password_entry = Entry(width=22)
+password_entry.grid(column=1, row=3)
+
+
+window.mainloop()
+
+
